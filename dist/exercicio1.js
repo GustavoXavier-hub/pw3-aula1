a3d7a618ea71441086722f1c477c8120831a06e9
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-08-02 20:37:46 -0300

    Exercicio 1 - TYPESCRIPT

---
+++ dist/exercicio1.js
@@ -0,0 +1,12 @@
+var nota1 = 10;
+var nota2 = 5;
+var nota3 = 9;
+var nota4 = 11;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A media é  " + media);
+if (media >= 7) {
+    console.log("APROVADO");
+}
+else {
+    console.log("REPROVADO");
+}
